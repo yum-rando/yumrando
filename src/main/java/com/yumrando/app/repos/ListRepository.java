d3d1f374e864169b30f309554aa12df41fb2881d
Rocco Paccione <leonardo.rocco.paccione@gmail.com>
2020-12-11 20:58:30 -0600

    feat: finished listed repos on kanban board

---
+++ src/main/java/com/yumrando/app/repos/ListRepository.java
@@ -0,0 +1,7 @@
+package com.yumrando.app.repos;
+
+import com.yumrando.app.models.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ListRepository extends JpaRepository<List, Long> {
+}
