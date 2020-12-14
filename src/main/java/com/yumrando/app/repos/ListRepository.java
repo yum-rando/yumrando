48d9f683d04a57dc18698455ce74058564cd3cfa
Chelsea <chelsea.e.rolland@gmail.com>
2020-12-14 12:00:54 -0600

    uncommented the repos

--- src/main/java/com/yumrando/app/repos/ListRepository.java
+++ src/main/java/com/yumrando/app/repos/ListRepository.java
@@ -1,7 +1,7 @@
-//package com.yumrando.app.repos;
-//
-//import com.yumrando.app.models.ListRestaurant;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface ListRepository extends JpaRepository<ListRestaurant, Long> {
-//}
+package com.yumrando.app.repos;
+
+import com.yumrando.app.models.ListRestaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ListRepository extends JpaRepository<ListRestaurant, Long> {
+}
