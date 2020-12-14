dd91091f6952b1905a24d8fed3fdc46e2492f8f5
Chelsea <chelsea.e.rolland@gmail.com>
2020-12-14 12:23:57 -0600

    deleted the ListRepo

--- src/main/java/com/yumrando/app/repos/ListRepository.java
+++
@@ -1,7 +0,0 @@
-package com.yumrando.app.repos;
-
-import com.yumrando.app.models.ListRestaurant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ListRepository extends JpaRepository<ListRestaurant, Long> {
-}
