464f1b5497913f61772ee5c0e678b3e82560de0e
Roberto <roberto.cuyar44@gmail.com>
2020-12-14 09:15:11 -0600

    Changed the handling of local storage in creating list items

--- src/main/resources/static/js/geoLocation.js
+++ src/main/resources/static/js/geoLocation.js
@@ -1,9 +1,10 @@
+"use strict"
 const geoLocation = () => {
     if (localStorage.getItem("yumCoord") === null) {
         window.navigator.geolocation.getCurrentPosition(
             ({coords})=> {
-                let place = { latitude: coords.latitude, longitude: coords.longitude}
-                localStorage.setItem("yumCoord", JSON.stringify(place));
+                localStorage.setItem("yumCoord",
+                    JSON.stringify({ latitude: coords.latitude, longitude: coords.longitude}));
             }
         )
     }
---
+++ src/main/resources/static/js/guestAddOn.js
@@ -0,0 +1,34 @@
+($ => {
+    $(document).ready(()=>{
+        const listBasic = array => {
+           let parent = $('#restaurant-items');
+           parent.empty();
+           array.map(item =>{
+               $(parent).append(
+                   `<h6 id="${item.name}">${item.name}</h6>`
+               );
+           })
+       }
+
+        const arrayConstructor = () => {
+            let listDisplay= localStorage.getItem("yumList");
+            if (listDisplay === null){
+                return [];
+            } else {
+               return JSON.parse(listDisplay);
+            }
+                }
+
+       $('#add-basic').click(()=>{
+           let basicInput = $('#simple-name');
+        localStorage.setItem("yumList", JSON.stringify(
+            [...arrayConstructor(), {name: basicInput.val()}]
+        ))
+         basicInput.val("");
+        listBasic(arrayConstructor());
+        })
+
+    listBasic(arrayConstructor());
+
+    })
+})(jQuery);
