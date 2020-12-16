25a577df785ee09982f7380d9f0e200af6a72855
Roberto <roberto.cuyar44@gmail.com>
2020-12-16 14:03:20 -0600

    Merged security with corresponding forms

--- src/main/java/com/yumrando/app/controllers/UserController.java
+++ src/main/java/com/yumrando/app/controllers/UserController.java
@@ -30,28 +30,35 @@ public class UserController {
         return "user/register";
     }
 
+//    @PostMapping("/register")
+//    public String saveUser(@ModelAttribute User user, @RequestParam (name = "confirmPassword") String checkPassword){
+//        if(user.getPassword().equals(checkPassword)){
+//            System.out.println(user.getPassword());
+//            String hash = passwordEncoder.encode(user.getPassword());
+//            user.setPassword(hash);
+//            users.save(user);
+//            return "redirect:/index"; // If password equals confirmPassword redirect to index
+//        }else{
+//            return "redirect:/register";
+//        }
+
     @PostMapping("/register")
-    public String saveUser(@ModelAttribute User user, @RequestParam (name = "confirmPassword") String checkPassword){
-        if(user.getPassword().equals(checkPassword)){
-            String hash = passwordEncoder.encode(user.getPassword());
-            user.setPassword(hash);
-            users.save(user);
-            return "redirect:/index"; // If password equals confirmPassword redirect to index
-        }else{
-            return "redirect:/register";
-        }
+    public String saveUser(@RequestParam String username, @RequestParam String password){
+        User test = new User();
+        test.setUsername(username);
+        test.setPassword(password);
+                System.out.println(test.getPassword());
+                String hash = passwordEncoder.encode(test.getPassword());
+                test.setPassword(hash);
+                users.save(test);
+                return "redirect:/index"; // If password equals confirmPassword redirect to index
+
 
     }
 
     @GetMapping("/profile")
     public String showProfile() {
         return "user/profile";
-    }
-
-    @GetMapping("/login")
-    @ResponseBody
-    public String showLogInPage() {
-        return "Login form goes here";
     }
 
     @PostMapping("/logout")
