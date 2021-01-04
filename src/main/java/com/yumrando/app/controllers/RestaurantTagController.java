8a44304f05dcc04bb28fca379b57d198d93dda4b
Rocco Paccione <leonardo.rocco.paccione@gmail.com>
2021-01-04 10:46:42 -0600

    feat: added userTagCreate

--- src/main/java/com/yumrando/app/controllers/RestaurantTagController.java
+++ src/main/java/com/yumrando/app/controllers/RestaurantTagController.java
@@ -1,10 +1,17 @@
 package com.yumrando.app.controllers;
 
+import com.yumrando.app.models.RestaurantTag;
+import com.yumrando.app.models.User;
 import com.yumrando.app.repos.ListRestaurantRepository;
 import com.yumrando.app.repos.RestaurantRepository;
 import com.yumrando.app.repos.TagRepository;
 import com.yumrando.app.repos.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RestaurantTagController {
@@ -20,6 +27,15 @@ public class RestaurantTagController {
         this.tagDao = tagDao;
         this.restaurantDao = restaurantDao;
     }
+
+    @GetMapping("/Tag/{id}")
+    public String restTagCreate(Model model, @PathVariable long id) {
+        User authenticUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        RestaurantTag userTag = tagDao.findByIdAndAndUsers(id, authenticUser);
+        model.addAttribute("userCreatedTag", userTag.getName());
+        return "partialsFile/userTagsList";
+    }
+    @PostMapping("")
 
 
 }
--- src/main/java/com/yumrando/app/repos/TagRepository.java
+++ src/main/java/com/yumrando/app/repos/TagRepository.java
@@ -2,10 +2,12 @@ package com.yumrando.app.repos;
 
 import com.yumrando.app.models.Restaurant;
 import com.yumrando.app.models.RestaurantTag;
+import com.yumrando.app.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TagRepository extends JpaRepository <RestaurantTag, Long> {
     RestaurantTag findById(long id);
+    RestaurantTag findByIdAndAndUsers(long id, User user);
 
     RestaurantTag save(RestaurantTag tag);
 
