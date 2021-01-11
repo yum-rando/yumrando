018b9bfff9ca92807680881d8c4b7d59bd6080b7
Chelsea <chelsea.e.rolland@gmail.com>
2021-01-11 17:19:47 -0600

    beginning to work on the integration jUnit testing

--- src/test/java/com/yumrando/app/AppApplicationTests.java
+++ src/test/java/com/yumrando/app/AppApplicationTests.java
@@ -1,13 +1,95 @@
-//package com.yumrando.app;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class AppApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+package com.yumrando.app;
+
+import com.yumrando.app.models.Restaurant;
+import com.yumrando.app.models.Review;
+import com.yumrando.app.models.User;
+import com.yumrando.app.repos.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.servlet.http.HttpSession;
+
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = AppApplication.class)
+@AutoConfigureMockMvc
+public class AppApplicationTests {
+
+    private User testUser;
+    private HttpSession httpSession;
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    UserRepository userDao;
+
+    @Autowired
+    ListRestaurantRepository listDao;
+
+    @Autowired
+    RestaurantRepository restaurantDao;
+
+    @Autowired
+    TagRepository tagDao;
+
+    @Autowired
+    ReviewRepository reviewDao;
+
+    @Autowired
+    UserTagRepository userTagDao;
+
+    @Autowired
+    PhotoRepository photoDao;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+    @Before
+    public void setup() throws Exception {
+
+        testUser = userDao.findByUsername("testUser");
+
+        //Creates the testUser if not exists
+        if (testUser == null){
+            User newUser = new User();
+            newUser.setUsername("testUser");
+            newUser.setPassword(passwordEncoder.encode("pass"));
+            newUser.setEmail("testUser@yumrando.com");
+            testUser = userDao.save(newUser);
+        }
+
+        // Throws a Post request to /login and expect a redirection to the yumRando homepage after being logged in
+        httpSession = this.mvc.perform(post("/login").with(csrf())
+                .param("username", "testUser")
+                .param("password", "pass"))
+                .andExpect(status().is(HttpStatus.FOUND.value()))
+                .andExpect(redirectedUrl("/"))
+                .andReturn()
+                .getRequest()
+                .getSession();
+    }
+
+    @Test
+    void contextLoads() {
+        // Sanity Test, just to make sure the MVC bean is working
+        assertNotNull(mvc);
+    }
+
+}
