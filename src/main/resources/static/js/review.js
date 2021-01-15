b6b4845dfea16053de3fef2bab9bd3f69c2bb719
Chelsea <chelsea.e.rolland@gmail.com>
2021-01-15 11:33:55 -0600

    cleaned up code for the review page and added separate line for the names and submit btn and photos

--- src/main/resources/static/js/review.js
+++ src/main/resources/static/js/review.js
@@ -1,9 +1,6 @@
 ($ => {
     "use strict";
 
-    // $('#review-comment-edit').click(() => {
-    //     $('#add-photo').toggleClass('d-none');
-    // })
     $('#photo-edit').click(() => {
         $('.delete-photo-list').toggleClass('d-none');
     })
