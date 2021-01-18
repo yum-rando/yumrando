dd2ae26b9754fef80b1efcde0111b1c9cbe55bb3
Roberto <roberto.cuyar44@gmail.com>
2021-01-17 21:04:47 -0600

    Finished basic modal styling and work on favorite tags modal.:

--- src/main/resources/static/js/friend.js
+++ src/main/resources/static/js/friend.js
@@ -19,6 +19,17 @@
             window.location.assign(`/friend/${friendId}/list/${listNum}`);
         })
 
+        const nullWebCheck = response =>{
+            if(typeof response === "string" && response !== ""){
+                return  `<div class="col-12">
+                    <p>
+                    <a class="modal-address" href="${response}" target="_blank">More Info</a>
+                </p>
+                </div>`
+            }
+            return "";
+        }
+
         $('.user-restaurants').click(function () {
             let restId = $(this).attr("id").substring(1);
             const friendId = $("#friend-label").attr('data-id');
@@ -34,11 +45,7 @@
                            <div class="col-12">
                                <p class="modal-address">${response.address}</p>
                            </div>
-                            <div class="col-12">
-                                <p>
-                                    <a class="modal-address" href="${response.website}" target="_blank">More Info</a>
-                                </p>
-                            </div>
+                               ${nullWebCheck(response.website)}
                             <div class="col-12">
                                 <p>
                                     <a class="modal-address" href="/list/${listId}/restaurant/${response.id}/review?friend=${friendId}">Review</a>
